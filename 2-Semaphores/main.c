cd7f6c92c556cdf185553f69b8f6c3c8e991eaa8
LucasGabrielfl0 <lucasgabrielfl00@gmail.com>
2025-05-25 12:11:28 -0300

    adding Semaphore example

--- 2-Semaphores/main.c
+++ 2-Semaphores/main.c
@@ -3,3 +3,38 @@
 * 2.2 Binary Sempahores
 * 2.3 Counting Semaphores
 */
+#include <stdio.h>
+#include "esp_log.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+
+void task1()
+{
+
+}
+
+void task2()
+{
+
+}
+
+void task_core1()
+{
+
+}
+
+void task_core2()
+{
+
+}
+
+void app_main() 
+{
+    // Tasks
+    xTaskCreate(&task1, "FastTask", 2048, "Task1", 3, NULL );
+    xTaskCreate(&task2, "SlowTask", 2048, "Task2", 1, NULL );
+
+
+}
